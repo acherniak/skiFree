0f67220114b7da83f452b479d9759580679dfb67
Alex Cherniak <alex.cherniak@vaporstream.com>
2021-04-14 07:05:15 -0400

    ready.md

---
+++ READY.md
@@ -0,0 +1,19 @@
+* **Fix a bug:**
+
+...
+
+Expected Result: The skier gets up and is facing to the left. 
+Also fixed the right arrow behaviour
+
+* **Write unit tests:**
+
+See the Game.test.js
+
+* **Extend existing functionality:**
+
+Implemented animated JUMP with space key and ramps
+
+* **Build something new:**
+
+Rhino appears after 5 minutes and chases the skier.
+Didn't go too far since Obstacles gets too many objects and app slows down
--- src/Core/Game.test.js
+++ src/Core/Game.test.js
@@ -1,5 +1,27 @@
 import "babel-polyfill";
+import { Skier } from "../Entities/Skier";
+import { Rhino } from "../Entities/Rhino";
+import * as Constants from "../Constants";
+import { Rect, intersectTwoRects } from './Utils';
 
-test('true equals true', () => {
-    expect(true).toBe(true);
+test('Skier created', () => {
+	let skier = new Skier(30,40);
+	expect(skier.x).toBe(30);
+	expect(skier.y).toBe(40);
+	expect(skier.assetName).toMatch(/^skier/)
 });
+
+test('Rhino created', () => {
+	let rhino = new Rhino(-30,-40);
+	expect({x: rhino.x, y: rhino.y, assetName: rhino.assetName}).toEqual({x:-30, y: -40, assetName: 'rhino'});
+});
+
+test('Constants are there', () => {
+	expect(Object.keys(Constants.ASSETS).length).toBeGreaterThan(10);
+	expect(Constants.SKIER_DIRECTIONS.DOWN).toBe(3);
+})
+
+test('Rectangles intersect', () => {
+	let rect1 = new Rect(10, 20, 30, 40), rect2 = new Rect(15, 25, 35, 45);
+	expect(intersectTwoRects(rect1,rect2)).toBe(true);
+})
